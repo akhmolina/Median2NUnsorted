ab0bcb28d2170a18423e78c395cb3b2d74f29d5b
akhmolina <valentina.ahmolina@yandex.ru>
2015-10-11 15:21:20 +0300

    Check for not digits at the start and the end of the input string added.

---
+++ src/Calculator.java
@@ -0,0 +1,43 @@
+public abstract class Calculator {
+
+	
+	public static long GetKElementOnLR ( long[] numbers, int k, int l, int r) 
+			throws ArrayIndexOutOfBoundsException
+	{
+
+		if (numbers.length <= r | l>r | l<0)
+		{throw new ArrayIndexOutOfBoundsException
+			("Массив integers " + 
+					" не включает в себя отрезок поиска [" + l + "," + r + "]");}
+		if (numbers.length <= k | k<0)
+		{throw new ArrayIndexOutOfBoundsException
+			("Массив integers не содержит элементов с индексом k = " + k + ".");}
+		
+		long x = numbers[k]; // x - начало поиска
+		int i = l;
+		int j = r;
+		
+		while (i<=j )
+		{
+			while (i< numbers.length && numbers[i]< x){i++;}
+			while (j>=0 && numbers[j]> x){j--;}
+
+			if (i<j)
+			{
+				long swap = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = swap;
+				i++; 
+				j--;
+
+			}
+			else if (i==j){i++; j--;}
+		}
+
+		if(l<=k && k<=j){return GetKElementOnLR(numbers, k, l, j);}
+		else if( i<=k && k<=r){return GetKElementOnLR(numbers, k, i, r);}
+	    
+		return numbers[k];
+	}
+
+}
---
+++ src/MedianCalculator.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MedianCalculator {
+
+	public static void main(String[] args) {
+		String[] firstNarray = null, secondNarray=null;
+		int N =0, N1 = 0;
+		
+		Scanner in = new Scanner(System.in);		
+		do
+		{
+			System.out.println("Введите первый массив N натуральных чисел, разделенных любым символом (одной строкой):");
+	        String firstN = in.nextLine();
+	        System.out.println("Введите второй массив N натуральных чисел, разделенных любым символом (одной строкой):");
+	        String secondN = in.nextLine();
+	        
+	        //убираем начальные и конечные нечисловые символы
+	        Pattern p = Pattern.compile("^\\D*((\\d+\\D+)*\\d+)\\D*$");  
+	        Matcher firstm = p.matcher(firstN);
+	        Matcher secondm = p.matcher(secondN);
+	        
+	        if (firstm.matches()){firstN = firstm.group(1);}
+	        
+	        if (secondm.matches()){secondN = secondm.group(1);}
+	        
+			firstNarray = firstN.split("\\D+");
+			secondNarray = secondN.split("\\D+");
+			N = firstNarray.length;
+			N1 = secondNarray.length;
+			
+			if (N <=1 || N1<=1)
+			{System.out.println("Массивы должны состоять из как минимум двух чисел. Введите массивы заново.");}
+			if (N != N1)
+			{System.out.println("Введенные массивы имеют не одинаковый размер. Введите массивы заново.");}
+ 
+	    }while (N<=1 || N != N1);	
+		in.close();
+		
+		
+		long[] numbers = new long[2*N];
+		for (int i = 0; i<= N-1; i++)
+		{numbers[i] = Long.parseLong(firstNarray[i]);}
+		for (int i = 0; i<= N-1; i++)
+		{numbers[N+i] = Long.parseLong(secondNarray[i]);}
+		
+		try{
+			double firstend = Calculator.GetKElementOnLR(numbers, N-1, 0, 2*N-1);
+			double secondstart = Calculator.GetKElementOnLR(numbers, N,  0, 2*N-1);
+			double median = (firstend + secondstart)/2;
+			System.out.println("Медиана равна = " + median);
+		}
+		catch (Exception ex)
+		{System.out.println(ex.getMessage());}
+		
+		
+	}
+
+}
